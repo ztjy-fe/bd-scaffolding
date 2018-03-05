e6e506c557b9eee9f4895a16586af862f97ab424
ztjy-fe <zhangzhenglong@szy.cn>
2018-03-05 11:29:46 +0800

    update meta.js

--- README.md
+++ README.md
@@ -1,21 +1,26 @@
-# szygulp-template
+# szyadmin-template
 
-> A minimal gulp template for pc project
+> A minimal vue admin template with Element UI & axios & permission control & lint
 
 ## Build Setup
 
 ``` bash
 
 # vue-cli init
-vue init ztjy-fe/szygulp-template project-name
+vue init ztjy-fe/szyadmin-template project-name
 
 # Clone project
-git clone https://github.com/ztjy-fe/szygulp-template.git
+git clone https://github.com/ztjy-fe/szyadmin-template.git
 
 # Install dependencies
 npm install
 
+# serve with hot reload at localhost:8586
+npm run dev
+
 # build for production with minification
 npm run build
 
+# build for production and view the bundle analyzer report
+npm run build --report
 ```
---
+++ meta.js
@@ -0,0 +1,50 @@
+module.exports = {
+
+  "helpers": {
+    "if_or": function (v1, v2, options) {
+      if (v1 || v2) {
+        return options.fn(this);
+      }
+
+      return options.inverse(this);
+    }
+  },
+  "prompts": {
+    "name": {
+      "type": "string",
+      "required": true,
+      "message": "Project name"
+    },
+    "description": {
+      "type": "string",
+      "required": false,
+      "message": "Project description",
+      "default": "A Common Gulp project"
+    },
+    "author": {
+      "type": "string",
+      "message": "Author"
+    },
+    "type": {
+      "type": "list",
+      "message": "select type for your project",
+      "choices": [
+        {
+          "name": "A PC project form common usage",
+          "value": "pc",
+          "short": "pc"
+        },
+        {
+          "name": "A Mobile project with WeiXin JS-SDK and Moblink",
+          "value": "mobile",
+          "short": "mobile"
+        }
+      ]
+    }
+  },
+  "filters": {
+    "src/script/jquery-1.12.4.min.js": "type === 'pc'",
+    "src/script/zepto-1.2.0.min.js": "type === 'mobile'",
+  },
+  "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://github.com/ztjy-fe/szyadmin-template"
+};
